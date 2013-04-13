788e18d6f87567881de0961a08b5303e5d01c47b
Michael Hayes <michael.hayes@canterbury.ac.nz>
2013-04-14 10:41:24 +1200

    Rename i2cm to i2c_master

--- apps/i2cm_test1/i2cm_test1.c
+++ apps/i2cm_test1/i2cm_test1.c
@@ -3,20 +3,20 @@
    Date:   13 April 2013
    Descr: 
 */
-#include "i2cm.h"
+#include "i2c_master.h"
 
 
 #define SDA_PIO PIO_DEFINE(PORT_A, 8)
 #define SCL_PIO PIO_DEFINE(PORT_A, 9)
 
-static const i2cm_bus_cfg_t i2c_bus_cfg =
+static const i2c_bus_cfg_t i2c_bus_cfg =
 {
     .scl = SCL_PIO,
     .sda = SDA_PIO
 };
 
 
-static const i2cm_dev_cfg_t i2c_dev1_cfg =
+static const i2c_slave_cfg_t i2c_slave1_cfg =
 {
     .id = 0x32,
     .addr_bytes = 1,
@@ -26,18 +26,18 @@ static const i2cm_dev_cfg_t i2c_dev1_cfg =
 int
 main (void)
 {
-    i2cm_t i2c_dev1;
+    i2c_t i2c_slave1;
 
-    i2c_dev1 = i2cm_init (&i2c_bus_cfg, &i2c_dev1_cfg);
+    i2c_slave1 = i2c_master_init (&i2c_bus_cfg, &i2c_slave1_cfg);
 
     while (1)
     {
         uint8_t tx[] = {1, 2, 3, 4};
         uint8_t rx[4];
 
-        i2cm_write (i2c_dev1, 25, tx, sizeof(tx));
+        i2c_master_write (i2c_slave1, 25, tx, sizeof(tx));
 
-        i2cm_read (i2c_dev1, 25, rx, sizeof(rx));
+        i2c_master_read (i2c_slave1, 25, rx, sizeof(rx));
     }
 }
 
