b0a7298a57d5dc7ad513880161bc26c7c0515c21
Michael Hayes <michael.hayes@canterbury.ac.nz>
2013-04-16 15:29:51 +1200

    Ensure usb_cdc_update called

--- apps/usb_cdc_test1/usb_cdc_test1.c
+++ apps/usb_cdc_test1/usb_cdc_test1.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include "usb_cdc.h"
 #include "sys.h"
+#include "pacer.h"
+
+#define PACER_RATE 1000
+
+#define SPAM_RATE 5
 
 int main (void)
 {
+    int ticks = 0;
+    int count = 0;
+
     usb_cdc_t usb_cdc;
 
     usb_cdc = usb_cdc_init ();
@@ -12,8 +20,21 @@ int main (void)
     sys_redirect_stdout ((void *)usb_cdc_write, usb_cdc);
     sys_redirect_stderr ((void *)usb_cdc_write, usb_cdc);
 
+    pacer_init (PACER_RATE);
+
     while (1)
     {
-        printf ("Hello world!\n");
+        pacer_wait ();
+
+        /* This needs to be periodically called to poll the USB
+           connection.  */
+        usb_cdc_update ();
+        
+        ticks++;
+        if (ticks > PACER_RATE / SPAM_RATE)
+        {
+            ticks = 0;
+            printf ("Hello world %u!\n\r", count++);
+        }
     }
 }
