80c90b61bc4736550095d55c51b3fe77ce1d8639
Michael Hayes <michael.hayes@canterbury.ac.nz>
2013-04-21 22:10:04 +1200

    Add rough demo

---
+++ apps/tcm8230_test1/tcm8230_test1.c
@@ -0,0 +1,16 @@
+/* File:   tcm8230_test1.c
+   Author: M. P. Hayes, UCECE
+   Date:   21 Apr 2013
+   Descr: 
+*/
+#include "tcm8230.h"
+
+
+int
+main (void)
+{
+    tcm8230_init ();
+
+    while (1)
+        continue;
+}
