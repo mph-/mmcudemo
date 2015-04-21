aa29fb9ce7e31ed7e7b4c0e1bbf977b91f154e79
Michael Hayes <michael.hayes@canterbury.ac.nz>
2015-04-21 21:25:05 +1200

    Tweak apps

--- apps/sleep_test1/sleep_test1.c
+++ apps/sleep_test1/sleep_test1.c
@@ -1,9 +1,9 @@
 /* File:   sleep_test1.c
    Author: M. P. Hayes, UCECE
    Date:   15 April 2013
-   Descr: 
+   Descr:  This only works for them SAM7 and needs tweaking for the SAM4S.
 */
-#include "cpu.h"
+#include "mcu.h"
 #include "led.h"
 #include "pio.h"
 #include "pacer.h"
@@ -49,8 +49,8 @@ sleep_setup (void)
 
     extint_sleep (extint);
     
-    /* Force a CPU reset.  */
-    cpu_reset ();
+    /* Force a MCU reset.  */
+    mcu_reset ();
 
     /* Never get here.  */
     while (1)
--- boards/board.mk
+++ boards/board.mk
@@ -4,7 +4,9 @@ OPT = -Os
 
 MMCULIB_DIR = ../../mmculib
 
+ifndef BOARD
 BOARD = sam4sbasic
+endif
 
 include ../../boards/$(BOARD)/board.mk
 include $(MMCULIB_DIR)/mmculib.mk
